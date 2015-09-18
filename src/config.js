3f2b8dddb0d513bc06bb0921ba397c9bdf332db6
Eric Smith <esmith@8thlight.com>
2015-09-18 18:23:54 -0500

    Create empty form
    This is an empty form that takes simple mortgage information and - well it does nothing on submit.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 System.config({
-  baseURL: baseURL,
+  baseURL: "/",
   defaultJSExtensions: true,
   transpiler: "babel",
   babelOptions: {
@@ -16,7 +16,9 @@ System.config({
   map: {
     "babel": "npm:babel-core@5.8.24",
     "babel-runtime": "npm:babel-runtime@5.8.20",
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
     "core-js": "npm:core-js@1.1.4",
+    "css": "github:systemjs/plugin-css@0.1.16",
     "jasmine": "npm:jasmine@2.3.2",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -49,6 +51,9 @@ System.config({
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
